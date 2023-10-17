097911ad894674a1dfeb97a7c6767566501fe78e
Lawal Abdulhameed Aremu <lawalabdulhameed91@gmail.com>
2023-10-17 15:13:22 -0700

    work

--- _printf.c
+++ _printf.c
@@ -4,40 +4,40 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
+ * @fmt: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int _printf(const char *fmt, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
+	if (fmt == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(list, fmt);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; fmt && fmt[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (fmt[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = fmt[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			/* write(1, &fmt[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			flags = get_flags(fmt, &i);
+			width = get_width(fmt, &i, list);
+			precision = get_precision(fmt, &i, list);
+			size = get_size(fmt, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handle_print(fmt, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -64,3 +64,4 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
+
